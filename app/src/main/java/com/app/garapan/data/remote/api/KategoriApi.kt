8d82b46589cbc929f33cf3d429c02c71fa842ee0
Andika Rafa Akbar <andikarafa1221@gmail.com>
2026-06-23 00:38:35 +0700

    fix(kategori): parse category list response

--- app/src/main/java/com/app/garapan/data/remote/api/KategoriApi.kt
+++ app/src/main/java/com/app/garapan/data/remote/api/KategoriApi.kt
@@ -1,9 +1,9 @@
 package com.app.garapan.data.remote.api
 
-import com.app.garapan.data.remote.dto.KategoriListResponseDto
+import com.app.garapan.data.remote.dto.KategoriDto
 import retrofit2.http.GET
 
 interface KategoriApi {
     @GET("kategori")
-    suspend fun getKategoriList(): KategoriListResponseDto
+    suspend fun getKategoriList(): List<KategoriDto>
 }
